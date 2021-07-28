74922cbf144befaa8e166d01f06d9e75af2125b2
unknown <emadhanna@gmail.com>
2021-07-28 12:53:34 -0400

    Adding code for updating LinearSearch and addimg BinarySearch

---
+++ src/BinarySearch.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		List<Character> resultedWord = addCharactersToList();
+		
+		System.out.println("You typed:");
+		printWordCaptured(resultedWord);
+	}
+
+
+	private static List<Character> addCharactersToList() {
+		List<Character> resultedWord = new ArrayList<>();
+		char capturedLetter = binaryLetter();
+		while (capturedLetter != '!') {
+			resultedWord.add(capturedLetter);
+			capturedLetter = binaryLetter();
+		}
+		return resultedWord;
+	}
+
+	public static char binaryLetter() {
+		
+		char ALPHABETLIST[] = {' ', '!', '.', 'a', 'b', 'c', 'd', 'e' , 'f', 'g', 'h', 
+				'i', 'j' , 'k' , 'l', 'm' , 'n', 'o', 'p', 'q' , 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		return checkCharacterRange(ALPHABETLIST, 0, ALPHABETLIST.length - 1);
+	}
+	
+	public static char checkCharacterRange(char[] alphabetList, int startValue, int endValue) {
+		int middleCharacter = extractMiddleValue(startValue, endValue);
+		boolean answerCaptured = false;
+		
+		if (startValue == endValue) {
+			answerCaptured = isThisTheLetter(alphabetList[startValue], alphabetList[endValue]);
+			if (answerCaptured) {
+				return alphabetList[startValue];
+			}else {
+				binaryLetter();
+			}
+		}
+		
+		answerCaptured = isThisTheLetter(alphabetList[startValue], alphabetList[middleCharacter]);
+		if (answerCaptured) {
+			return checkCharacterRange(alphabetList, startValue, middleCharacter);
+		}
+		
+		return checkCharacterRange(alphabetList, middleCharacter + 1, endValue);
+	}
+
+
+	private static int extractMiddleValue(int startCharacter, int endCharacter) {
+		int middleCharacter = startCharacter + (endCharacter - startCharacter) / 2;
+		return middleCharacter;
+	}
+	
+	public static boolean isThisTheLetter(char startCharcter, char endCharacter) {
+		Scanner scan = new Scanner(System.in);
+		validateStartAndEndCharacterNotMatched(startCharcter, endCharacter);
+		char capteredAnswer = 'n';
+		String capturedWord = scan.next();
+		capturedWord = capturedWord.toLowerCase();
+		capteredAnswer = capturedWord.charAt(0);
+		
+		if (capteredAnswer == 'y') {
+			return true;
+		}else {
+			while(capteredAnswer != 'n') {
+				validateStartAndEndCharacterNotMatched(startCharcter, endCharacter);
+				capturedWord = scan.next();
+				capturedWord = capturedWord.toLowerCase();
+				capteredAnswer = capturedWord.charAt(0);
+			}
+			return false;
+		}
+	}
+
+	private static void validateStartAndEndCharacterNotMatched(char startCharcter, char endCharacter) {
+		if (startCharcter != endCharacter) {
+			System.out.println("Is your letter between '" + startCharcter + "' and '" + endCharacter + "'?");
+		}else {
+			System.out.println("Is your letter '" + startCharcter + "'?");
+		}
+	}
+	
+	public static void printWordCaptured(List<Character> list) {
+		
+		for (Character character : list) {
+			System.out.print(character);
+		}
+	}
+
+}
--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -58,37 +58,6 @@ public class LinearSearch {
 		}
 	}
 	
-//	public static List<Character> checkLetter(char[] alphabetList, int start) {
-//		
-//		List<Character> resultedWord = new ArrayList<Character>();
-//		Scanner scan = new Scanner(System.in);
-//		
-//		for (int counter=start; counter < alphabetList.length; counter++) {
-//			System.out.println("Are you thinking of the letter '" + alphabetList[counter] + "'?");
-//			char capteredAnswer = 'n';
-//			String capturedWord = scan.next();
-//			capturedWord = capturedWord.toLowerCase();
-//			capteredAnswer = capturedWord.charAt(0);
-//			
-//			if(capteredAnswer == 'y') {
-//				if (alphabetList[counter] == '!') {
-//					return resultedWord;
-//				}else {
-//					resultedWord.add(alphabetList[counter]);
-//					checkLetter(alphabetList, start);
-//				}
-//			}else {
-//				while(capteredAnswer != 'n') {
-//					System.out.println("Are you thinking of the letter '" + alphabetList[counter] + "'?");
-//					capturedWord = scan.next();
-//					capturedWord = capturedWord.toLowerCase();
-//					capteredAnswer = capturedWord.charAt(0);
-//				}
-//			}
-//		}
-//		return resultedWord;
-//	}
-	
 	public static void printWordCaptured(List<Character> list) {
 		
 		for (Character character : list) {
